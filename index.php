91a890bbedebb9f45b526287b7f7bd6187bf6e3f
rasyidalfaiz <rasyidalfaiz73@gmail.com>
2024-06-20 01:00:41 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+echo "<center>nama_lengkap: ".$_POST['nama_lengkap']."</center><br>";
+echo "<center>tanggal_lahir: ".$_POST['tanggal_lahir']."</center><br>";
+echo "<center>alamat_rumah: ".$_POST['alamat_rumah']."</center><br>";
+echo "<center>kota: ".$_POST['kota']."</center><br>";
+echo "<center>Cabang_Olahraga_Pilihan: ".$_POST['Cabang_Olahraga_Pilihan']."</center><br>";
+?>
